50e471b9e86ab90e815b09568975a0edd4983e03
User <User@DESKTOP-EIOC2SN>
2023-02-03 15:17:56 -0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PvZHCardEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindCardButton_Click(object sender, RoutedEventArgs e)
+        {
+            var item = new TreeViewItem
+            {
+                Header = "Test"
+            };
+            CardDataView.Items.Add(item);
+        }
+    }
+}
